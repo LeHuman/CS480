74703b2c1a5c65e7e1e0ca3b80f08a60186ce5ab
LeHuman <47549331+LeHuman@users.noreply.github.com>
2022-10-17 17:43:52 -0500

    no worky

--- cs480_P01_A.py
+++ cs480_P01_A.py
@@ -1,102 +1,93 @@
 import argparse
-from dataclasses import dataclass, field
-from typing import Any, Callable, Iterable
 import pandas as pd
 from queue import PriorityQueue
 
 
-@dataclass(order=True)
-class edge():
-    val: int
-    state: object = field(compare=False)
-
-    def __init__(self, state: object, val: int) -> None:
-        self.state = state
-        self.val = val
-
-    def __hash__(self):
-        return hash(self.state)
-
-    def __str__(self) -> str:
-        return f"{str(self.state)}:{str(self.val)}"
+DMEMO: dict[str, object] = {}
 
 
 class node():
-    uid: str = None
-    edges: list[edge] = None
+    name: str = None
+    edges: dict[str, int] = None
+    dists: dict[str, int] = None
 
-    def __init__(self, uid) -> None:
-        self.uid = uid
-        self.edges = list()
+    def __init__(self, name: str) -> None:
+        self.name = name
+        self.edges = {}
+        self.dists = {name: 0}
 
-    def connect(self, neighbor, value) -> None:
-        self.edges.append(edge(neighbor, value))
-        self.edges.sort()  # Helpful for visualize
+    def connect(self, node_o, dist: int, isNeighbor: bool) -> None:
+        self.dists[node_o.name] = dist
+        if isNeighbor:
+            self.edges[node_o.name] = dist
 
     def __hash__(self):
-        return hash(self.uid)
+        return hash(self.name)
 
     def __str__(self) -> str:
-        return str(self.uid)
+        return str(self.name)
 
     def __repr__(self) -> str:
-        return f"{self.uid}:{[str(x) for x in self.edges]}"
+        return str(self)
 
 
-DMEMO: dict[str, node] = {}
-SMEMO: dict[str, node] = {}
+def get_node(name: str) -> node:
+    if name in DMEMO:
+        return DMEMO[name]
+    DMEMO[name] = node(name)
+    return DMEMO[name]
 
 
-def get_dnode(state: str) -> node:
-    global DMEMO
-    return DMEMO[state]
+# def best_first_search(problem, f):
+#     node = Node(problem.initial)
+#     frontier = PriorityQueue([node], key=f)
+#     reached = {problem.initial: node}
+#     while frontier:
+#         node = frontier.pop()
+#         if problem.is_goal(node.state):
+#             return node
+#         for child in expand(problem, node):
+#             s = child.state
+#             if s not in reached or child.path_cost < reached[s].path_cost:
+#                 reached[s] = child
+#                 frontier.add(child)
+#     return failure
 
 
-def get_snode(state: str) -> node:
-    global SMEMO
-    return SMEMO[state]
-
-# Initial: MA | Goal: MD | Path: ['MA', 'NY', 'PA', 'MD']
-# Path cost: 575 miles
-
-
-def run_greedy(initial: str, goal: str) -> list[node]:
-    initial_state = get_dnode(initial)
-    goal_state = get_dnode(goal)
-    goal_h = get_snode(goal_state.uid).edges
+def run_greedy(initial: str, goal: str) -> tuple[int, list[node]]:
+    initial_state = get_node(initial)
+    goal_state = get_node(goal)
     queue = PriorityQueue()
-    visited_e: dict[str, edge] = {}
-    visited_le: list[edge] = list()
-    queue.put(edge(initial_state, goal_h[initial_state.uid]))
+    visited_e: dict[str, tuple[int, int, node]] = {}
+    explored_e: dict[str, tuple[int, int, node]] = {}
+    visited_le: list[node] = list()
+    cost = 0
+    visited_e[initial_state.name] = (initial_state.dists[goal_state.name], 0, initial_state)
+    explored_e[initial_state.name] = visited_e[initial_state.name]
+    queue.put(visited_e[initial_state.name])
     while not queue.empty():
-        curr_edge: edge = queue.get()
-        curr_state: node = curr_edge.state
-        visited_e[curr_state.uid] = curr_edge
-        visited_le.append(curr_edge)
-        if curr_state is goal_state:
+        curr_node: node = queue.get()[2]
+        visited_le.append(curr_node)
+        if curr_node is goal_state:
             break
-        for next_edge in curr_state.edges:
-            next_state_uid = next_edge.state.uid
-            val = goal_h[next_state_uid] + next_edge.val
-            if next_state_uid not in visited_e or val < visited_e[next_state_uid].val + goal_h[next_state_uid]:
-                queue.put((val, next_edge))
-                visited_e[next_state_uid] = next_edge
-    return visited_le
+        for name, dist in curr_node.edges.items():
+            h = goal_state.dists[name] + dist
+            node_o = (h, dist, get_node(name))
+            if name not in visited_e or h < explored_e[name][0]:  # check for less than last visit?
+                visited_e[name] = node_o
+                queue.put(node_o)
+                explored_e[name] = node_o
+    cost = sum(visited_e[k.name][1] for k in visited_le)
+    return (cost, visited_le)
 
 
-def gen_graph(df: pd.DataFrame, memo: dict[str, node]):
-    def get_node(key):
-        if key in memo:
-            return memo[key]
-        memo[key] = node(key)
-        return memo[key]
-
+def gen_graph(df: pd.DataFrame, neighbors: bool):
     for i, s in df.iloc[:, 1:].items():
         s = pd.concat([df[df.columns[0]], s], axis=1)
         s = s[s[s.columns[1]] > 0]
         nd = get_node(i)
         for i, v in s.iterrows():
-            nd.connect(get_node(v[0]), v[1])
+            nd.connect(get_node(v[0]), v[1], neighbors)
 
 
 def main() -> None:
@@ -112,12 +103,11 @@ def main() -> None:
     df: pd.DataFrame = pd.read_csv("driving.csv").convert_dtypes(int)
     sf: pd.DataFrame = pd.read_csv("straightline.csv").convert_dtypes(int)
 
-    global DMEMO
-    global SMEMO
-    gen_graph(df, DMEMO)
-    gen_graph(sf, SMEMO)
+    gen_graph(df, True)
+    gen_graph(sf, False)
 
-    print(", ".join([str(x) for x in run_greedy(args.initial_state[0], args.goal_state[0])]))
+    cost, path = run_greedy(args.initial_state[0], args.goal_state[0])
+    print(cost, ":", ", ".join([str(x) for x in path]))
 
 
 if __name__ == "__main__":
